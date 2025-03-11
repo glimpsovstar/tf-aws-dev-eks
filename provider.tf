1aa1c55083791b94ea8798b7abb4d5b4e9d91a7a
David Joo <david.joo@hashicorp.com>
2025-03-12 09:33:16 +1100

    Refactored provider configuration, updated data.tf remote state name

--- provider.tf
+++ provider.tf
@@ -7,6 +7,19 @@ terraform {
   }
 }
 
+terraform {
+  cloud {
+    organization = "djoo-hashicorp"
+    workspaces {
+      name = "tf-aws-dev-eks"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 provider "aws" {
   region = var.aws_region
 }
