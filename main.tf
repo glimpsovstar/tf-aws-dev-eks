b83883c9d007ed9cbcda0ffec52877818d2e0849
David Joo <david.joo@hashicorp.com>
2025-03-12 19:26:44 +1100

    Updated EKS module version to 20.34.0 to fix missing cluster_id

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "20.34.0"
   cluster_name    = var.eks_cluster_name
   cluster_version = var.eks_cluster_version
 
