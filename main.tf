a8f338940fd00e39515da395da3a4af2c93ab5bc
David Joo <david.joo@hashicorp.com>
2025-06-27 20:16:01 +1000

    Update EKS module version to 20.37.1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.34.0"
+  version         = "20.37.1"
   cluster_name    = var.eks_cluster_name
   cluster_version = var.eks_cluster_version
   enable_cluster_creator_admin_permissions = true
