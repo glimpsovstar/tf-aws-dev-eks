0ec13f6dd2a6679e3680867997cb65698eabda38
David Joo <david.joo@hashicorp.com>
2025-07-10 11:19:57 +1000

    Comment out EKS cluster data sources in provider.tf for clarity and future reference

--- provider.tf
+++ provider.tf
@@ -28,13 +28,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
+#data "aws_eks_cluster" "cluster" {
+#  name = module.eks.cluster_name
+#}
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
+#data "aws_eks_cluster_auth" "cluster" {
+#  name = module.eks.cluster_name
+#}
 
 provider "kubernetes" {
 #  host                   = data.aws_eks_cluster.cluster.endpoint
